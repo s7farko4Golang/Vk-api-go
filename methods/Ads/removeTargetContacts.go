7cef6261f54f438a123bcce83e3465cc3159dd8d
s7farko4golang <s7farko3@gmail.com>
2025-09-04 16:12:42 +0300

    v0.0.1 Добавлен метод ads.removeTargetContacts

---
+++ methods/Ads/removeTargetContacts.go
@@ -0,0 +1,52 @@
+package Ads
+
+import (
+	"Vk-api-go/types"
+	"context"
+	"net/url"
+	"strconv"
+)
+
+type RemoveTargetContactsOptions struct {
+	accountID     int    //Идентификатор рекламного кабинета.
+	clientID      int    //Только для рекламных агентств. Id клиента, в рекламном кабинете которого находится аудитория.
+	targetGroupID int    //Bдентификатор аудитории таргетинга.
+	contacts      string //Список телефонов, email адресов или идентификаторов пользователей, указанных через запятую.
+	// Также принимаются их MD5-хеши.
+}
+
+type RemoveTargetContactsOption func(*RemoveTargetContactsOptions)
+
+func RemoveTargetContactsWithClientID(clientID int) RemoveTargetContactsOption {
+	return func(o *RemoveTargetContactsOptions) {
+		o.clientID = clientID
+	}
+}
+
+// RemoveTargetContacts Принимает запрос на исключение контактов рекламодателя из аудитории ретаргетинга.
+// Для вызова метода можно использовать:
+// •ключ доступа пользователя (требуются права доступа: ads)
+func (am *AddMethods) RemoveTargetContacts(ctx context.Context, accountID int, targetGroupID int, contacts string, opts ...RemoveTargetContactsOption) (types.VkResponse, error) {
+
+	options := &RemoveTargetContactsOptions{}
+
+	for _, opt := range opts {
+		opt(options)
+	}
+
+	params := url.Values{}
+
+	params.Set("account_id", strconv.Itoa(accountID))
+	params.Set("client_id", strconv.Itoa(targetGroupID))
+	params.Set("contacts", contacts)
+
+	if options.clientID != 0 {
+		params.Set("client_id", strconv.Itoa(options.clientID))
+	}
+
+	VkRequest := types.VkRequest{
+		Method: "ads.removeTargetContacts",
+		Params: params,
+	}
+	return am.methods.Call(ctx, VkRequest)
+}
