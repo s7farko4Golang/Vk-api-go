985a104b9e384270fd3c036d89d078f46a3c60e5
s7farko4golang <s7farko3@gmail.com>
2025-09-04 16:38:05 +0300

    v0.0.1 Добавлен метод ads.updateClients

--- methods/Ads/updateClients.go
+++ methods/Ads/updateClients.go
@@ -9,7 +9,7 @@ import (
 	"strconv"
 )
 
-type CreateClientOrdSubagent struct {
+type UpdateClientOrdSubagent struct {
 	OrdSubagentType string `json:"type"` //Тип подрядчика. Возможные значения:
 	//   • person — физическое лицо.
 	//   • individual — индивидуальный предприниматель.
@@ -20,7 +20,7 @@ type CreateClientOrdSubagent struct {
 }
 
 // OrdDataSpecification Объект описывает оператора рекламных данных.
-type CreateClientOrdDataSpecification struct {
+type UpdateClientOrdDataSpecification struct {
 	ClientType string `json:"client_type"` //Тип клиента. Возможные значения:
 	//• person — физическое лицо.
 	//• individual — индивидуальный предприниматель.
@@ -29,7 +29,7 @@ type CreateClientOrdDataSpecification struct {
 	Inn            string                  `json:"inn"`             //ИНН клиента. Длина строки: 10 или 12 символов.
 	Phone          string                  `json:"phone"`           //Телефон клиента. Длина строки: от 7 до 20 символов.
 	AgencyPhone    string                  `json:"agency_phone"`    //Номер телефона агентства. Длина строки: от 7 до 20 символов. Доступно с версии 5.194
-	Subagent       CreateClientOrdSubagent `json:"subagent"`        //Данные о подрядчике в формате JSON (ord_subagent).
+	Subagent       UpdateClientOrdSubagent `json:"subagent"`        //Данные о подрядчике в формате JSON (ord_subagent).
 	ContractNumber string                  `json:"contract_number"` //Номер контракта.
 	ContractDate   string                  `json:"contract_date"`   //Дата контракта в формате DD.MM.YYYY.
 	ContractType   string                  `json:"contract_type"`   //Тип контракта. Примеры:
@@ -40,23 +40,23 @@ type CreateClientOrdDataSpecification struct {
 	WithVat        bool   `json:"with_vat"`        //Информация о том, подлежит ли НДС оплате в рамках этого контракта.
 }
 
-// CreateClientsSpecification Объект описывает клиента рекламного агентства.
-type CreateClientsSpecification struct {
+// UpdateClientsSpecification Объект описывает клиента рекламного агентства.
+type UpdateClientsSpecification struct {
 	Name     string                           `json:"name"`      //Название клиента. Длина строки: от 3 до 60 символов.
 	DayLimit int                              `json:"day_limit"` //Дневной лимит в рублях.
 	AllLimit int                              `json:"all_limit"` //Общий лимит в рублях.
-	OrdData  CreateClientOrdDataSpecification `json:"ord_data"`  //Данные оператора рекламных данных в формате JSON (ord_data_specification).
+	OrdData  UpdateClientOrdDataSpecification `json:"ord_data"`  //Данные оператора рекламных данных в формате JSON (ord_data_specification).
 }
 
-// CreateClientsRequest структура для передачи в функцию
-type CreateClientsRequest struct {
-	CreateClientsSpecification []CreateClientsSpecification `json:"client_specification"`
+// UpdateClientsRequest структура для передачи в функцию
+type UpdateClientsRequest struct {
+	UpdateClientsSpecification []UpdateClientsSpecification `json:"client_specification"`
 }
 
-// CreateClientsSerialize сериализует массив CreateClientsSpecification в JSON
-func CreateClientsSerialize(CreateClientsSpecification []CreateClientsSpecification) (string, error) {
-	request := CreateClientsRequest{
-		CreateClientsSpecification: CreateClientsSpecification,
+// UpdateClientsSerialize сериализует массив UpdateClientsSpecification в JSON
+func UpdateClientsSerialize(UpdateClientsSpecification []UpdateClientsSpecification) (string, error) {
+	request := UpdateClientsRequest{
+		UpdateClientsSpecification: UpdateClientsSpecification,
 	}
 
 	jsonData, err := json.Marshal(request)
@@ -67,15 +67,15 @@ func CreateClientsSerialize(CreateClientsSpecification []CreateClientsSpecificat
 	return string(jsonData), nil
 }
 
-// CreateClients Метод создаёт клиентов рекламного агентства. Доступен только для рекламных агентств.
+// UpdateClients Метод создаёт клиентов рекламного агентства. Доступен только для рекламных агентств.
 // Для вызова метода можно использовать:
 // •ключ доступа пользователя (требуются права доступа: ads)
-func (am *AddMethods) CreateClients(ctx context.Context, accountID int, data string) (types.VkResponse, error) {
+func (am *AddMethods) UpdateClients(ctx context.Context, accountID int, data string) (types.VkResponse, error) {
 	params := url.Values{}
 	params.Set("account_id", strconv.Itoa(accountID))
 	params.Set("data", data)
 	VkRequest := types.VkRequest{
-		Method: "ads.createClients",
+		Method: "ads.updateClients",
 		Params: params,
 	}
 	return am.methods.Call(ctx, VkRequest)
