bd5540718d5c32ad768a3b3c73e8af783290dfcd
s7farko4golang <s7farko3@gmail.com>
2025-09-04 13:05:39 +0300

    v0.0.1 Добавлен методы getCampaigns.go, getCategories.go

---
+++ methods/Ads/getCategories.go
@@ -0,0 +1,43 @@
+package Ads
+
+import (
+	"Vk-api-go/types"
+	"context"
+	"net/url"
+)
+
+type GetCategoriesOptions struct {
+	lang string
+}
+
+type GetCategoriesOption func(*GetCategoriesOptions)
+
+func GetCategoriesWithLang(lang string) GetCategoriesOption {
+	return func(o *GetCategoriesOptions) {
+		o.lang = lang
+	}
+}
+
+// GetCategories Позволяет получить возможные тематики рекламных объявлений.
+// Для вызова метода можно использовать:
+// •ключ доступа пользователя (требуются права доступа: ads)
+func (am *AddMethods) GetCategories(ctx context.Context, opts ...GetCategoriesOption) (types.VkResponse, error) {
+
+	options := &GetCategoriesOptions{}
+
+	for _, opt := range opts {
+		opt(options)
+	}
+
+	params := url.Values{}
+
+	if options.lang != "" {
+		params.Set("lang", options.lang)
+	}
+
+	VkRequest := types.VkRequest{
+		Method: "ads.getCategories",
+		Params: params,
+	}
+	return am.methods.Call(ctx, VkRequest)
+}
