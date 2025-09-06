19712e15da8a6636bec3c81496ef9d394b471ba6
s7farko4golang <s7farko3@gmail.com>
2025-09-07 02:31:34 +0300

    v0.0.1 Добавлен метод apps.promoHasActiveGift.

---
+++ methods/Apps/promoHasActiveGift.go
@@ -0,0 +1,47 @@
+package Apps
+
+import (
+	"Vk-api-go/types"
+	"context"
+	"net/url"
+	"strconv"
+)
+
+type PromoHasActiveGiftOptions struct {
+	promoId uint //Идентификатор промо-акции.
+	userId  uint //Идентификатор пользователя. Используется только при запросе с сервисным токеном.
+}
+
+type PromoHasActiveGiftOption func(*PromoHasActiveGiftOptions)
+
+func PromoHasActiveGiftWithPromoId(promoId uint) PromoHasActiveGiftOption {
+	return func(o *PromoHasActiveGiftOptions) {
+		o.promoId = promoId
+	}
+}
+
+// PromoHasActiveGift Проверить есть ли у пользователя подарок в игре.
+// Для вызова метода можно использовать:
+// •ключ доступа пользователя
+// •сервисный ключ доступа
+func (am *AppMethods) PromoHasActiveGift(ctx context.Context, promoId uint, opts ...PromoHasActiveGiftOption) (types.VkResponse, error) {
+
+	options := &PromoHasActiveGiftOptions{}
+
+	for _, opt := range opts {
+		opt(options)
+	}
+
+	params := url.Values{}
+	params.Set("promo_id", strconv.Itoa(int(promoId)))
+
+	if options.userId != 0 {
+		params.Set("user_id", strconv.Itoa(int(options.userId)))
+	}
+
+	VkRequest := types.VkRequest{
+		Method: "apps.promoHasActiveGift",
+		Params: params,
+	}
+	return am.methods.Call(ctx, VkRequest)
+}
