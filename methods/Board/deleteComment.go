57396db9658c014da9a92ffc9add1ab025834f2f
s7farko4golang <s7farko3@gmail.com>
2025-09-07 03:16:15 +0300

    v0.0.1 Добавлен метод board.editComment.

--- methods/Board/deleteComment.go
+++ methods/Board/deleteComment.go
@@ -4,17 +4,18 @@ import (
 	"Vk-api-go/types"
 	"context"
 	"net/url"
+	"strconv"
 )
 
 // DeleteComment Удаляет сообщение темы в обсуждениях сообщества.
 // Для вызова метода можно использовать:
 // •ключ доступа пользователя, полученный в Standalone‑приложении через Implicit Flow (требуются права доступа: groups)
 // •ключ доступа сообщества
-func (am *BoardMethods) DeleteComment(ctx context.Context, groupId string, topicId string, commentId string) (types.VkResponse, error) {
+func (am *BoardMethods) DeleteComment(ctx context.Context, groupId uint, topicId uint, commentId string) (types.VkResponse, error) {
 
 	params := url.Values{}
-	params.Set("group_id", groupId)
-	params.Set("topic_id", topicId)
+	params.Set("group_id", strconv.Itoa(int(groupId)))
+	params.Set("topic_id", strconv.Itoa(int(topicId)))
 	params.Set("comment_id", commentId)
 
 	VkRequest := types.VkRequest{
---
+++ methods/Board/editComment.go
@@ -0,0 +1,69 @@
+package Board
+
+import (
+	"Vk-api-go/types"
+	"context"
+	"net/url"
+	"strconv"
+)
+
+type EditCommentOptions struct {
+	groupId     uint   //Идентификатор сообщества, в котором размещено обсуждение.
+	topicId     uint   //Идентификатор обсуждения.
+	commentId   string //Идентификатор комментария в обсуждении.
+	message     string //Новый текст комментария (является обязательным, если не задан параметр attachments).
+	attachments string //Список объектов, приложенных к записи, в формате:
+	//<type><owner_id>_<media_id>,<type><owner_id>_<media_id>
+	//<type> — тип медиа-приложения:
+	//• photo — фотография;
+	//• video — видеозапись ;
+	//• audio — аудиозапись;
+	//• doc — документ.
+	//<owner_id> — идентификатор владельца медиа-приложения. <media_id> — идентификатор медиа-приложения.
+	//Например:
+	//photo100172_166443618,photo66748_265827614
+	//Параметр является обязательным, если не задан параметр text.
+}
+
+type EditCommentOption func(*EditCommentOptions)
+
+func EditCommentWithMessage(message string) EditCommentOption {
+	return func(o *EditCommentOptions) {
+		o.message = message
+	}
+}
+func EditCommentWithAttachments(attachments string) EditCommentOption {
+	return func(o *EditCommentOptions) {
+		o.attachments = attachments
+	}
+}
+
+// EditComment Редактирует одно из сообщений в обсуждении сообщества.
+// Для вызова метода можно использовать:
+// •ключ доступа пользователя, полученный в Standalone‑приложении через Implicit Flow (требуются права доступа: groups)
+func (am *BoardMethods) EditComment(ctx context.Context, groupId uint, topicId uint, commentId string, opts ...EditCommentOption) (types.VkResponse, error) {
+
+	options := &EditCommentOptions{}
+
+	for _, opt := range opts {
+		opt(options)
+	}
+
+	params := url.Values{}
+	params.Set("group_id", strconv.Itoa(int(groupId)))
+	params.Set("topic_id", strconv.Itoa(int(topicId)))
+	params.Set("comment_id", commentId)
+	// Add parameters to values based on options
+	if options.message != "" {
+		params.Set("message", options.message)
+	}
+	if options.attachments != "" {
+		params.Set("attachments", options.attachments)
+	}
+
+	VkRequest := types.VkRequest{
+		Method: "board.editComment",
+		Params: params,
+	}
+	return am.methods.Call(ctx, VkRequest)
+}
